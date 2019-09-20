300735afc8826fe9db7cf3da467352677deb0231
ijayantsingh <jayant.singh_cs18@gla.ac.in>
2019-09-20 22:07:53 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int a ;
+        String c ,d ,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the book number :");
+        a = sc.nextInt();
+        if (a== 0) {
+            System.out.println("N/A");
+        }
+        else{
+            while (a -- >0){
+                sc.skip("\n");
+                System.out.println(" enter the Book name: ");
+                c = sc.nextLine();
+                System.out.println("enter the author name :");
+                d = sc.nextLine();
+                System.out.println("enter the ISBN number ");
+                b = sc.nextLine();
+                String m = "-----------------------------";
+                System.out.println(m);
+                System.out.println("Book Name : \t" +c);
+                System.out.println("Author Name : \t"+d);
+                System.out.println("ISBN  : \t"+b);
+                System.out.println(m);
+            }
+        }
+    }
+}
